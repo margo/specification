bfd26f47a7d8bbcabcc0304ff9a709a2088d8ad8
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2026-03-17 22:21:49 +0100

    add tool to generate the json schemas
    Add tool to generate JSON-Schemas to validate instances of the top-level
    data types:
    - ApplicationDeployment
    - ApplicationDescription
    - DesiredStateManifest
    - DeviceCapabilities
    
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

---
+++ data-model/tools/generate-json-schemas.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+THIS_SCRIPT="$(readlink -f "${0}")"
+THIS_DIR="$(dirname "${THIS_SCRIPT}")"
+
+ROOT_DIR="$(dirname "$(dirname "${THIS_DIR}")")"
+
+TGT_DIR="${ROOT_DIR}/generated/json-schemas"
+
+if command -v poetry &>/dev/null; then
+  RUN="poetry run"
+else
+  if ! command -v linkml &>/dev/null; then
+    echo "The command 'linkml' is missing"
+    exit 1
+  fi
+  RUN=""
+fi
+
+mkdir -p "${TGT_DIR}"
+
+for schema in "application-deployment" "application-description" "desired-state-manifest" "device-capabilities"; do
+  ${RUN} linkml generate json-schema "${ROOT_DIR}/data-model/${schema}.linkml.yaml" >"${TGT_DIR}/${schema}.schema.json"
+done
