b2ab7abccadd984c481e8033cc8893cc5ee48b02
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2026-03-18 12:42:57 +0100

    improve diagram generation
    Add class-relationships that cannot be modelled with LinkML and add
    possibility to generate multiple class-focused diagrams.
    
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- data-model/tools/generate-class-diagram.bash
+++ data-model/tools/generate-class-diagram.bash
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+TMP_PLANTUML_FILE=$(mktemp)
+
+cleanup() {
+  rm "${TMP_PLANTUML_FILE}"
+}
+
+trap cleanup EXIT
+
 set -eu
 
 THIS_SCRIPT="$(readlink -f "${0}")"
@@ -9,6 +17,91 @@ ROOT_DIR="$(dirname "$(dirname "${THIS_DIR}")")"
 
 TGT_DIR="${ROOT_DIR}/generated/diagrams"
 
+if command -v poetry &>/dev/null; then
+  RUN="poetry run"
+else
+  if ! command -v linkml &>/dev/null; then
+    echo "The command 'linkml' is missing"
+    exit 1
+  fi
+  if ! command -v curl &>/dev/null; then
+    echo "The command 'curl' is missing"
+    exit 1
+  fi
+  RUN=""
+fi
+
 mkdir -p "${TGT_DIR}"
 
-linkml generate plantuml --directory "${TGT_DIR}" --format svg "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml"
+# Holistic class diagram
+${RUN} linkml generate plantuml "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml" |
+  sed "s/@enduml/DeploymentAnnotations ..> ApplicationDescription\n@enduml/" |
+  sed "s/@enduml/DeploymentStatusManifest ..> ApplicationDeployment\n@enduml/" |
+  sed "s/@enduml/DesiredStateManifest ..> ApplicationDeployment\n@enduml/" >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/DataModel-ClassDiagram.svg"
+
+if [ "$#" -lt 1 ]; then
+  exit
+fi
+
+# Class diagram focused on ApplicationDescription
+${RUN} linkml generate plantuml \
+  --classes ApplicationDescription \
+  --classes ApplicationMetadata \
+  --classes Parameter \
+  --classes Configuration \
+  --classes DeploymentProfileDescription \
+  "${ROOT_DIR}/data-model/application-description.linkml.yaml" |
+  sed "/Component.*{$/,/}/d" \
+    >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/ApplicationDescription-ClassDiagram.svg"
+
+# Class diagram focused on DeploymentStatusManifest
+${RUN} linkml generate plantuml \
+  --classes DeploymentStatusManifest \
+  --classes ApplicationDeployment \
+  --classes DeploymentProfile \
+  "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml" |
+  sed "s/@enduml/DeploymentStatusManifest ..> ApplicationDeployment\n@enduml/" |
+  sed "s/@enduml/ComponentStatus ..> Component\n@enduml/" \
+    >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/DeploymentStatusManifest-ClassDiagram.svg"
+
+# Class diagram focused on DesiredStateManifest
+${RUN} linkml generate plantuml \
+  --classes DesiredStateManifest \
+  "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml" |
+  sed "s/@enduml/DesiredStateManifest ..> ApplicationDeployment\n@enduml/" \
+    >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/DesiredStateManifest-ClassDiagram.svg"
+
+# Class diagram focused on DeviceCapabilities
+${RUN} linkml generate plantuml \
+  --classes DeviceCapabilitiesManifest \
+  --classes Properties \
+  --classes Resources \
+  "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml" \
+  >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/DeviceCapabilities-ClassDiagram.svg"
+
+# Class diagram focused on ApplicationDeployment
+${RUN} linkml generate plantuml \
+  --classes ApplicationDeployment \
+  --classes DeploymentMetadata \
+  --classes DeploymentAnnotations \
+  --classes Spec \
+  --classes Parameter \
+  --classes DeploymentProfile \
+  --classes ComposeDeploymentProfile \
+  --classes HelmDeploymentProfile \
+  --classes Component \
+  "${ROOT_DIR}/data-model/application-deployment.linkml.yaml" |
+  sed "s/@enduml/DeploymentAnnotations ..> ApplicationDescription\n@enduml/" \
+    >"${TMP_PLANTUML_FILE}"
+
+curl -H "Content-Type: test/plain" --data-binary @"${TMP_PLANTUML_FILE}" https://kroki.io/plantuml/svg -o "${TGT_DIR}/ApplicationDeployment-ClassDiagram.svg"
