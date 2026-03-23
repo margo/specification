68a2999c5483740b30f4a1cb7b18a6c18968791c
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2026-03-23 19:42:15 +0100

    improve data model documentation generation
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- data-model/tools/generate-docs.bash
+++ data-model/tools/generate-docs.bash
@@ -7,7 +7,13 @@ THIS_DIR="$(dirname "${THIS_SCRIPT}")"
 
 ROOT_DIR="$(dirname "$(dirname "${THIS_DIR}")")"
 
-TGT_DIR="${ROOT_DIR}/generated/markdown"
+TMP_DIR="$(mktemp -d)"
+
+cleanup() {
+  rm -r "${TMP_DIR}"
+}
+
+trap cleanup EXIT
 
 if command -v poetry &>/dev/null; then
   RUN="poetry run"
@@ -23,16 +29,47 @@ else
   RUN=""
 fi
 
+TGT_DIR="${ROOT_DIR}/generated/markdown_main-classes"
+
 mkdir -p "${TGT_DIR}"
+
+# Main classes
+
+for schema_name in "application-description" "application-deployment" "deployment-status" "desired-state-manifest" "device-capabilities"; do
+  ${RUN} linkml generate doc \
+    --directory="${TMP_DIR}" \
+    --template-directory="${ROOT_DIR}/data-model/resources/markdown-templates_main-classes" \
+    --preserve-names \
+    --stacktrace \
+    --example-directory="${ROOT_DIR}/data-model/resources/examples/valid" \
+    "${ROOT_DIR}/data-model/${schema_name}.linkml.yaml"
+
+  mv "${TMP_DIR}/index.md" "${TGT_DIR}/${schema_name}.md"
+  rm -rf "${TMP_DIR:?}/*"
+done
+
+TGT_DIR="${ROOT_DIR}/generated/markdown"
+
+mkdir -p "${TGT_DIR}"
+
+# Whole Data Model
 
 ${RUN} linkml generate doc \
   --directory="${TGT_DIR}" \
   --template-directory="${ROOT_DIR}/data-model/resources/markdown-templates" \
   --preserve-names \
+  --stacktrace \
   --example-directory="${ROOT_DIR}/data-model/resources/examples/valid" \
   "${ROOT_DIR}/data-model/margo-data-model.linkml.yaml"
 
 mkdir -p system-design/data-model
 mv generated/markdown/* system-design/data-model/
+
+"${THIS_DIR}/generate-class-diagram.bash"
+
+cp "${ROOT_DIR}/generated/diagrams/DataModel-ClassDiagram.svg" system-design/figures/
+cp "${ROOT_DIR}/generated/diagrams/DataModel-ClassDiagram.png" system-design/figures/
+
+"${THIS_DIR}/generate-openapi.bash"
 
 ${RUN} mkdocs build
